878bd04a2211f702123a391fea0a6b4db2cd1053
acer <acer@LAPTOP-ENM13016>
2024-09-18 08:27:35 +0700

    Tugas pekan1

---
+++ LaundryApps/src/Model/Costumer.java
@@ -0,0 +1,38 @@
+package Model;
+
+public class Costumer {
+
+	        String id, nama, alamat, nohp;
+	        public String getId() {
+	                return id;
+	        }
+	        public void setId(String id) {
+	                this.id = id;
+	        }
+	        public String getNama() {
+	                return nama;
+	        }
+	        public void setNama(String nama) {
+	                this.nama = nama;
+	        }
+	        public String getAlamat() {
+	                return alamat;
+	        }
+	        public void setAlamat(String alamat) {
+	                this.alamat = alamat;
+	        }
+	        public String getNohp() {
+	                return nohp;
+	        }
+	        public void setNohp(String nohp) {
+	                this.nohp = nohp;
+	        }
+	        public static void main(String[] args) {
+	                // TODO Auto-generated method stub
+
+	        }
+
+	
+
+	}
+
